6e243b40231a238973512f3196f8bd061f1dc237
datargs <37572894+datargs@users.noreply.github.com>
2025-06-17 01:48:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -97,6 +97,7 @@ one_line_style = JsCode("""
 # Cột biển số, ngày, chi phí: không wrap
 gb.configure_column("Biển số", wrapText=False, autoHeight=False, width=90, cellStyle=one_line_style)
 gb.configure_column("Ngày", wrapText=False, autoHeight=False, width=90, cellStyle=one_line_style)
+gb.configure_column("Nội dung", wrapText=False, autoHeight=False, width=90, cellStyle=one_line_style)
 gb.configure_column("Chi phí", wrapText=False, autoHeight=False, width=90, cellStyle=one_line_style)
 
 # Cột nội dung: 1 dòng, dấu ba chấm
